a82dfe74ef3aa1409886dd90b30dd643c3916d98
Brett Moore & Kyle Giard-Chase <student@epicodus.com>
2015-03-12 10:51:52 -0700

    add test for 2 character word

--- scrabble/tests/ScrabbleTest.php
+++ scrabble/tests/ScrabbleTest.php
@@ -17,6 +17,19 @@
             $score = array(1);
             $this->assertEquals($score, $result);
         }
+        function test_scrabbleScoreTwo()
+        {
+            //Arrange
+            $test_scrabble_score_2 = new Scrabble;
+            $input_word = "to";
+
+            //Act
+            $result = $test_scrabble_score_2->scrabbleScore($input_word);
+
+            //Assert
+            $score = array(2);
+            $this->assertEquals($score, $result);
+        }
     }
 
 ?>
