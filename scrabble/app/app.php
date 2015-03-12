ea5a122a33995f36337cf0635060aaef6756da8a
Brett Moore & Kyle Giard-Chase <student@epicodus.com>
2015-03-12 13:34:38 -0700

    Add home.twig file and create form for user input_word

--- scrabble/app/app.php
+++ scrabble/app/app.php
@@ -1,17 +1,17 @@
 <?php
 
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../src/MyProjectClass.php';
+    require_once __DIR__.'/../src/Scrabble.php';
 
     $app = new Silex\Application;
-    $app['debug'] = true;
+    //$app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path'=>__DIR__.'/../views'
     ));
 
     $app->get('/', function() use ($app) {
-        return $app['twig']->render('template.twig');
+        return $app['twig']->render('home.twig');
     });
 
     return $app;
