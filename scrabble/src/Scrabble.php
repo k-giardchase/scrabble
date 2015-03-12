4960cb22d5e3423f81171e75c9a5106bf14b501e
Brett Moore & Kyle Giard-Chase <student@epicodus.com>
2015-03-12 11:59:43 -0700

    add test and funcionality for varying 3 letter word values

--- scrabble/src/Scrabble.php
+++ scrabble/src/Scrabble.php
@@ -7,24 +7,32 @@
 
                     $score = array();
                     $word_split = array();
-                    $one = array("o", "t", "a");
+                    $one = array("s", "e", "i", "o", "u", "l", "n", "r", "a", "t");
                     $two = array ("g", "d");
+                    $three = array("b", "c", "m", "p");
                     $word_split = str_split($input_word);
-                    $tile_one = 0;
-                    $tile_two = 0;
+                    $tile = 0;
+
 
                     foreach($word_split as $letter) {
                         if (in_array($letter, $one)) {
-                             ++$tile_one;
-                             array_push($score, $tile_one);
-
+                             ++$tile;
+                             array_push($score, $tile);
+                             $tile = 0;
                         }
 
                         elseif(in_array($letter, $two)) {
-                            $tile_two += 2;
-                            array_push($score, $tile_two);
-
+                            $tile += 2;
+                            array_push($score, $tile);
+                            $tile = 0;
                         }
+
+                        elseif(in_array($letter, $three)) {
+                            $tile += 3;
+                            array_push($score, $tile);
+                            $tile = 0;
+                        }
+
 
                     }
 
--- scrabble/tests/ScrabbleTest.php
+++ scrabble/tests/ScrabbleTest.php
@@ -14,22 +14,41 @@
             $result = $test_Scrabble->scrabbleScore($input_word);
 
             //Assert
-            $i = 1;
-            $this->assertEquals($i, $result);
+            $final_score = 1;
+            $this->assertEquals($final_score, $result);
         }
         function test_scrabbleScoreTwo()
         {
             //Arrange
             $test_scrabble_score_2 = new Scrabble;
-            $input_word = "to";
+            $input_word = "go";
 
             //Act
             $result = $test_scrabble_score_2->scrabbleScore($input_word);
 
             //Assert
-            $i = 2;
-            $this->assertEquals($i, $result);
+            $final_score = 3;
+            $this->assertEquals($final_score, $result);
         }
+
+        function test_scrabbleScoreThree()
+        {
+            //Arrange
+            $test_scrabble_score_3 = new Scrabble;
+            $input_word = "bad";
+
+            //Act
+            $result = $test_scrabble_score_3->scrabbleScore($input_word);
+
+            //Assert
+            $final_score = 6;
+            $this->assertEquals($final_score, $result);
+        }
+
+
+
+
+
     }
 
 ?>
